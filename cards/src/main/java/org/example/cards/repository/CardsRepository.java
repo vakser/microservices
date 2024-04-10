15af695da9ccb85c5594cc3b008049fcc5a36759
vakser <65089446+vakser@users.noreply.github.com>
2024-04-10 10:47:55 +0300

    Cards microservice added

---
+++ cards/src/main/java/org/example/cards/repository/CardsRepository.java
@@ -0,0 +1,12 @@
+package org.example.cards.repository;
+
+import org.example.cards.entity.Cards;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CardsRepository extends JpaRepository<Cards, Long> {
+    Optional<Cards> findByMobileNumber(String mobileNumber);
+
+    Optional<Cards> findByCardNumber(String cardNumber);
+}
